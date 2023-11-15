257f5b282cfe36c59dd00538b2b0b2b5c03eaf0e
Marko <mancinomarko@hotmail.it>
2023-11-15 01:42:13 +0100

    fix: routes for setting desktop

--- routes.py
+++ routes.py
@@ -1,6 +1,6 @@
-from fastapi import APIRouter, status, HTTPException
+from fastapi import APIRouter, status, Request
 from fastapi.responses import JSONResponse
-from models import VirtualDesktopResponse
+from models import VirtualDesktopResponse, ElioDevice
 from utils import desktop_environment
 
 routes = APIRouter()
@@ -9,10 +9,22 @@ routes = APIRouter()
 def get_virtual_desktop():
     return VirtualDesktopResponse(actual=desktop_environment.get_current_desktop())
 
+@routes.post("/virtualdesktop")
+async def set_virtual_desktop(request: Request):
+    json_payload = await request.json()
 
-@routes.post("/virtualdesktop", response_model=VirtualDesktopResponse)
-def set_virtual_desktop(vd: int):
+    vd = int(json_payload['cubePosition'])
+    print("Set desktop to:", vd)
     if desktop_environment.switch_to_desktop(vd):
         return VirtualDesktopResponse(actual=vd)
     else:
         JSONResponse(status_code=status.HTTP_406_NOT_ACCEPTABLE)
+
+async def print_request(request: Request):
+    print(f'request header       : {dict(request.headers.items())}' )
+    print(f'request query params : {dict(request.query_params.items())}')  
+    try : 
+        print(f'request json         : {await request.json()}')
+    except Exception as err:
+        # could not parse json
+        print(f'request body         : {await request.body()}')
