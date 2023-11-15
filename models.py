909bdd8ebc3f0fb3650565e47905d9a1e56028ba
Marko <mancinomarko@hotmail.it>
2023-11-15 01:42:23 +0100

    added elio device as model

--- models.py
+++ models.py
@@ -7,3 +7,37 @@ class VirtualDesktopResponse(BaseModel):
     """
 
     actual: int
+
+class ElioDevice(BaseModel):
+    time: int
+    plugged: bool
+    wirelessCharging: bool
+    sunDetected: bool
+    batteryVoltage: float
+    tempC: float
+    tempF: float
+    humidity: float
+    tvoc: float
+    proximity: float
+    light: float
+    lightRed: int
+    lightGreen: int
+    lightBlue: int
+    IRlight: int
+    cubePosition: int
+    decahedronPosition: int
+    pressure: float
+    altitude: float
+    tempOutC: float
+    latitude: float
+    longitude: float
+    D1: bool
+    D2: bool
+    D3: bool
+    D4: bool
+    D5: bool
+    LED: int
+    version: str
+    MAC: str
+
+
